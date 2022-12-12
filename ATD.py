9146381fdb108fec1ef68b3a2209cd043378017e
Manitary <pak.ska@gmail.com>
2022-12-12 19:27:21 +0000

    Refactor get_entries() Grab entries with a single comprehension Remove transform_entries() as it is now deprecated Minor changes to the how entries are displayed on screen

--- ATD.py
+++ ATD.py
@@ -40,96 +40,62 @@ def display_anime(matching_anime_indexed: dict[int, dict[str, str]]) -> str:
             user_input = int(input())
         except ValueError:
             pass
+
     slug = matching_anime_indexed[user_input]["slug"]
     return slug
 
 
-# ===== GET ALL OP / ED WITH ASSOCIATED LINK ===== #
-def get_entries(slug: int) -> dict:
+def get_entries(slug: str) -> dict:
+    """Return the OPs/EDs associated to the given URL slug."""
+    # Currently, only return one version of each OP/ED (the highest quality available?)
     response = requests.get(
-        f"https://api.animethemes.moe/anime/{slug}?include=animethemes.animethemeentries.videos.audio"
+        f"https://api.animethemes.moe/anime/{slug}?include=animethemes.animethemeentries.videos.audio",
+        timeout=REQUEST_TIMEOUT,
     ).json()
-    raw_entries = []
-    for item in response["anime"]["animethemes"]:
-        name = item["animethemeentries"][0]["videos"][0].get("filename")
-        video_link = item["animethemeentries"][0]["videos"][0].get("link")
-        audio_link = item["animethemeentries"][0]["videos"][0]["audio"].get("link")
-        temp_list = []
-        temp_list.clear()
-        temp_list.append(name)
-        temp_list.append(video_link)
-        temp_list.append(audio_link)
-        name_link_tuple = tuple(temp_list)
-        raw_entries.append(name_link_tuple)
-
-    entries = tranform_entries(raw_entries)
-
+    entries = {
+        i: {
+            "name": entry["animethemeentries"][0]["videos"][0]["filename"],
+            "video-link": entry["animethemeentries"][0]["videos"][0]["link"],
+            "audio-link": entry["animethemeentries"][0]["videos"][0]["audio"]["link"],
+        }
+        for i, entry in enumerate(response["anime"]["animethemes"])
+    }
     return entries
 
 
-# ===== ===== #
-
-
-# ===== TRANFORM LISTS OF TUPLES raw_entries TO DICTIONARY ===== #
-
-
-def tranform_entries(raw_entries):
-    n = len(raw_entries)
-    indexes = [x for x in range(1, n + 1)]
-    entries = dict.fromkeys(indexes, {})
-    i = 0
-    for item in raw_entries:
-        i = i + 1
-        entries.update(
-            {i: {}}
-        )  # if i remove this line it stops working and i don't know why -_-
-        entries[i].update({"name": item[0]})
-        entries[i].update({"video-link": item[1]})
-        entries[i].update({"audio-link": item[2]})
-
-    return entries
-
-
-# ===== ===== #
-
-
-# ===== DISPLAY ===== #
-def display_videos(videos):
+def display_videos(videos: dict[int, dict[str, str]]) -> tuple[str, str]:
+    """Receive user input to select an entry and choose between video and audio; return the URL."""
     user_input = None
     while user_input not in videos.keys():
         os.system("cls" if os.name == "nt" else "clear")
-        for number, value in videos.items():
-            print(f"{number} -")
-            for name, link in value.items():
-                print(f"{name}: {link}")
+        for number, entry in videos.items():
+            print(f"{number} - {entry['name']}")
+            for name, link in entry.items():
+                if name != "name":
+                    print(f"{name}: {link}")
             print("=====")
-
-        print("Enter which entry you want to download (number):")
+        print("\n\nEnter which entry you want to download (number):")
         try:
             user_input = int(input())
         except ValueError:
             pass
 
+    file_name = videos[user_input].get("name")
     video_audio_choice = None
     while video_audio_choice not in (1, 2):
         os.system("cls" if os.name == "nt" else "clear")
+        print(f"{file_name}")
         print("1 - Video\n2 - Only audio")
         try:
             video_audio_choice = int(input())
         except ValueError:
             pass
 
-    if video_audio_choice == 1:
-        link_to_download = videos[user_input].get("video-link")
-    elif video_audio_choice == 2:
-        link_to_download = videos[user_input].get("audio-link")
-
-    file_name = videos[user_input].get("name")
+    link_to_download = videos[user_input][
+        "video-link" if user_input == 1 else "audio-link"
+    ]
 
     return link_to_download, file_name
-
-
-# ===== ===== #
 
 
 # ===== DOWNLOAD ===== #
